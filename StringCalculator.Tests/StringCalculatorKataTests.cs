111b126bdcc4b0fe43dd3bddbbffd4b5494eda04
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-03 15:08:46 +0300

    Refactored tests and added new one

--- StringCalculator.Tests/StringCalculatorKataTests.cs
+++ StringCalculator.Tests/StringCalculatorKataTests.cs
@@ -12,7 +12,7 @@ public class StringCalculatorKataTests
     }
 
     [Fact]
-    public void Add_OfEmptyString_IsZero()
+    public void Add_EmptyString_ReturnsZero()
     {
         var result = _stringCalculator.Add("");
         
@@ -20,23 +20,23 @@ public class StringCalculatorKataTests
     }
   
     [Fact]
-    public void Add_WithOneNumberAndComma_IsCorrect()
+    public void Add_SingleNumberAndSeparator_ReturnsNumber()
     {
         var result = _stringCalculator.Add("1,");
         
         Assert.Equal(1, result);
     }
-    
+
     [Fact]
-    public void Add_WithDifferentSeparators_IsCorrect()
+    public void Add_WithDifferentSeparators_ShouldReturnNumber()
     {
-        var result = _stringCalculator.Add("//;\n1;\n2,\\3");
+        var result = _stringCalculator.Add("//;\\n1;\\n2,\\3");
         
         Assert.Equal(6, result);
     }
     
     [Fact]
-    public void Add_WithNegativeNumbers_ThrowException()
+    public void Add_NumbersWithNegativeNumber_ThrowsException()
     {
         var exception = Assert.Throws<ArgumentException>(() => _stringCalculator.Add("-1,-3"));
         
@@ -44,10 +44,35 @@ public class StringCalculatorKataTests
     }
     
     [Fact]
-    public void Add_WithNumberBiggerThanOneThousands_IsCorrect()
+    public void Add_NumbersAboveUpperLimit_AreIgnored()
     {
         var result = _stringCalculator.Add("1,10000");
         
         Assert.Equal(1, result);
     }
+    
+    [Fact]
+    public void Add_NumbersWithCustomSeparator_ReturnsNumber()
+    {
+        var result = _stringCalculator.Add("//;\\n2;3");
+
+        Assert.Equal(5, result);
+    }
+    
+    [Fact]
+    public void Add_NumbersWithLongCustomSeparator_ReturnsNumber()
+    {
+        var result = _stringCalculator.Add("//[***]\\n1***2***3");
+
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void Add_NumbersWithSeveralCustomSeparators_ReturnsNumber()
+    {
+        var result = _stringCalculator.Add("//[*][%]\\n1*2%3");
+        
+        Assert.Equal(6, result);
+    }
+
 }
