372941da86dfe112c43044212cee4d04e6f54b5f
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-03 15:10:01 +0300

    Added logic for getting custom separator and redesigned IsAnyNegativeNumbers method

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,39 +1,66 @@
+using System.Text.RegularExpressions;
+
 namespace StringCalculator;
 
 public class StringCalculator
 {
-    public int Add(string numbers)
+    public int Add(string expression)
     {
-        if (string.IsNullOrEmpty(numbers))
+        const int upperLimit = 1000;
+        if (string.IsNullOrEmpty(expression))
         {
             return 0;
         }
 
-        var numbersAsInts = ParseNumbers(numbers);
-        numbersAsInts = RemoveNumbersBiggerThanOneThousands(numbersAsInts);
+        var numbersAsInts = ParseExpression(expression)
+            .Where(x => x <= upperLimit);
 
-        if (IsAnyNegativeNumbers(numbersAsInts, out var negativeNumbers))
-        {
-            throw new ArgumentException($"Negative numbers are not allowed ({string.Join(", ", negativeNumbers)})");
-        }
+        ThrowExceptionIfAnyNegativeNumbers(numbersAsInts);
 
         return numbersAsInts.Sum();
     }
+    
+    private IEnumerable<int> ParseExpression(string numbers)
+    {
+        var numbersToProcess = numbers;
+        var separators = new List<string> {",", "\\n", "\\"};
 
-    private int[] ParseNumbers(string numbers)
-    {
-        var separators = new[] { ',', ' ', '\n', ';', '\\', '/' , '[', ']', '*'};
+        var partWithSeparators = numbers.Split("\\n").First();
         
-        return numbers.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-    }
-    private bool IsAnyNegativeNumbers(IEnumerable<int> numbers, out IEnumerable<int> negativeNumbers)
-    {
-        negativeNumbers = numbers.Where(x => x < 0);
+        if(partWithSeparators.StartsWith("//"))
+        {
+            separators.AddRange(GetCustomSeparators(partWithSeparators));
+            numbersToProcess = numbers.Substring(partWithSeparators.Length + 2);
+        }
         
-        return negativeNumbers.Any();
+        return numbersToProcess.Split(separators.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x));
     }
-    private int[] RemoveNumbersBiggerThanOneThousands(IEnumerable<int> numbers)
+
+    private IEnumerable<string> GetCustomSeparators(string input)
     {
-        return numbers.Where(x => x < 1000).ToArray();
+        const int minimumCustomSeparatorLength = 3;
+        
+        var separators = Array.Empty<string>();
+
+        if (input.Length == minimumCustomSeparatorLength)
+        {
+            separators = new[] { input[minimumCustomSeparatorLength - 1].ToString() };
+        }
+        else
+        {
+            separators = input.Split(new[] { ']', '[' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        return separators;
+    }
+    private void ThrowExceptionIfAnyNegativeNumbers(IEnumerable<int> numbers)
+    {
+        var negativeNumbers = numbers.Where(x => x < 0);
+        
+        if (negativeNumbers.Any())
+        {
+            throw new ArgumentException($"Negative numbers are not allowed ({string.Join(", ", negativeNumbers)})");
+        }
     }
 }
