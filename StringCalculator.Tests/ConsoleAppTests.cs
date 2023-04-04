bb3b3c21d96be02276f6aa81015aa02c1fcc9861
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-04 16:57:07 +0300

    Removed redundant tests and rewrote old one

--- StringCalculator.Tests/ConsoleAppTests.cs
+++ StringCalculator.Tests/ConsoleAppTests.cs
@@ -1,4 +1,5 @@
 using Moq;
+using StringCalculator;
 using StringCalculator.Console;
 using Xunit;
 
@@ -7,24 +8,61 @@ namespace StringCalculatorTests;
 public class ConsoleAppTests
 {
     private readonly ConsoleApp _consoleApp;
-    private readonly Mock<IConsoleWrapper> _consoleWrapper;
+    private readonly Mock<ConsoleWrapper> _consoleWrapper;
+    private readonly Mock<Calculator> _calculator;
     public ConsoleAppTests()
     {
-        _consoleWrapper = new Mock<IConsoleWrapper>();
-        _consoleApp = new ConsoleApp(_consoleWrapper.Object);
+        _calculator = new Mock<Calculator>();
+        _consoleWrapper = new Mock<ConsoleWrapper>();
+
+        _consoleApp = new ConsoleApp(_consoleWrapper.Object, _calculator.Object);
     }
 
     [Fact]
-    public void Run_EmptyString_PrintNothing()
+    public void Run_EmptyString_PrintStartMessage()
     {
         // Arrange
         _consoleWrapper.Setup(x => x.ReadLine()).Returns("");
+        _calculator.Setup(x => x.Add("")).Returns(0);
         
         // Act
         _consoleApp.Run();
 
         // Assert
         _consoleWrapper.Verify(x => x.WriteLine("Enter comma separated numbers(enter to exit):"), Times.Once);
+    }
+    [Fact]
+    public void Run_OneNumber_PrintSecondTryMessage()
+    {
+        // Arrange
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
+            .Returns("1")
+            .Returns("");
+        _calculator.SetupSequence(x => x.Add(It.IsAny<string>()))
+            .Returns(1);
+
+        // Act
+        _consoleApp.Run();
+
+        // Assert
+        _consoleWrapper.Verify(x => x.WriteLine("you can enter other numbers (enter to exit)?"), Times.Once);
+    }
+    
+    [Fact]
+    public void Run_OneNumber_PrintNumber()
+    {
+        // Arrange
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
+            .Returns("1")
+            .Returns("");
+        _calculator.SetupSequence(x => x.Add(It.IsAny<string>()))
+            .Returns(1);
+
+        // Act
+        _consoleApp.Run();
+
+        // Assert
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
     }
 
     [Fact]
@@ -35,150 +73,16 @@ public class ConsoleAppTests
             .Returns("1")
             .Returns("2")
             .Returns("");
+        _calculator.SetupSequence(x => x.Add(It.IsAny<string>()))
+            .Returns(1)
+            .Returns(2);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Enter comma separated numbers(enter to exit):"), Times.Once);
-        _consoleWrapper.Verify(x => x.WriteLine("you can enter other numbers (enter to exit)?"), Times.Exactly(2));
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
         _consoleWrapper.Verify(x => x.WriteLine("Result: 2"), Times.Once);
     }
 
-    [Fact]
-    public void Run_OneNumber_PrintNumber()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("1")
-            .Returns("");
-
-        // Act
-        _consoleApp.Run();
-
-        // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
-    }
-
-    [Fact]
-    public void Run_WithDifferentSeparators_PrintSum()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("//;\\n1;\\n2,\\3")
-            .Returns("");
-
-        // Act
-        _consoleApp.Run();
-
-        // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
-    }
-
-    [Fact]
-    public void Run_NumbersWithNegativeNumber_ShouldThrowsException()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("-1,-3")
-            .Returns("");
-
-        // Act
-        var exception = Assert.Throws<ArgumentException>(() => _consoleApp.Run());
-
-        // Assert
-        Assert.Equal("Negative numbers are not allowed (-1, -3)", exception.Message);
-    }
-
-    [Fact]
-    public void Run_NumbersAboveUpperLimit_PrintSumWithoutNumberThatAboveLimit()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("1,10000")
-            .Returns("");
-
-        // Act
-        _consoleApp.Run();
-
-        // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
-
-    }
-
-    [Fact]
-    public void Run_NumbersWithCustomSeparator_PrintSum()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("//;\\n2;3")
-            .Returns("");
-
-        // Act
-        _consoleApp.Run();
-
-        // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 5"), Times.Once);
-    }
-
-    [Fact]
-    public void Run_NumbersWithCustomSeparatorAndNewLine_PrintSum()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("//;\\n2;3\\n4")
-            .Returns("");
-
-        // Act
-        _consoleApp.Run();
-
-        // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 9"), Times.Once);
-    }
-
-    [Fact]
-    public void Run_NumbersWithSeparatorOfAnyLength_PrintSum()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("//[***]\\n1***2***3")
-            .Returns("");
-
-        // Act
-        _consoleApp.Run();
-
-        // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
-    }
-
-    [Fact]
-    public void Run_NumbersWithMultipleCustomSeparators_PrintSum()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("//[*][%]\\n1*2%3")
-            .Returns("");
-        
-        // Act
-        _consoleApp.Run();
-
-        // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
-    }
-
-    [Fact]
-    public void Run_NumbersWithCustomSeparatorAsNestedBrackets_PrintSum()
-    {
-        // Arrange
-        _consoleWrapper.SetupSequence(x => x.ReadLine())
-            .Returns("//[**[**]\\n1**[**2**[**3")
-            .Returns("");
-
-        // Act
-        _consoleApp.Run();
-
-        // Assert
-        _consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
-    }
+  
 }
