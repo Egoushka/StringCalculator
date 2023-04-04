a1e636235eb639260e7739623d4aca0e358ab48c
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-04 16:33:35 +0300

    Moved initialization of objects in tests methods to constructor

--- StringCalculator.Tests/ConsoleAppTests.cs
+++ StringCalculator.Tests/ConsoleAppTests.cs
@@ -6,91 +6,83 @@ namespace StringCalculatorTests;
 
 public class ConsoleAppTests
 {
-    private ConsoleApp _consoleApp;
-
+    private readonly ConsoleApp _consoleApp;
+    private readonly Mock<IConsoleWrapper> _consoleWrapper;
     public ConsoleAppTests()
     {
+        _consoleWrapper = new Mock<IConsoleWrapper>();
+        _consoleApp = new ConsoleApp(_consoleWrapper.Object);
     }
 
     [Fact]
     public void Run_EmptyString_PrintNothing()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.Setup(x => x.ReadLine()).Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
-
+        _consoleWrapper.Setup(x => x.ReadLine()).Returns("");
+        
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Enter comma separated numbers(enter to exit):"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Enter comma separated numbers(enter to exit):"), Times.Once);
     }
 
     [Fact]
     public void Run_TwoInputs_PrintTwoResults()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("1")
             .Returns("2")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Enter comma separated numbers(enter to exit):"), Times.Once);
-        consoleWrapper.Verify(x => x.WriteLine("you can enter other numbers (enter to exit)?"), Times.Exactly(2));
-        consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
-        consoleWrapper.Verify(x => x.WriteLine("Result: 2"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Enter comma separated numbers(enter to exit):"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("you can enter other numbers (enter to exit)?"), Times.Exactly(2));
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 2"), Times.Once);
     }
 
     [Fact]
     public void Run_OneNumber_PrintResult()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("1")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
     }
 
     [Fact]
     public void Run_WithDifferentSeparators_PrintSum()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("//;\\n1;\\n2,\\3")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
     }
 
     [Fact]
     public void Run_NumbersWithNegativeNumber_ShouldThrowsException()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("-1,-3")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         var exception = Assert.Throws<ArgumentException>(() => _consoleApp.Run());
@@ -103,17 +95,15 @@ public class ConsoleAppTests
     public void Run_NumbersAboveUpperLimit_PrintSumWithoutNumberAboveLimit()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("1,10000")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 1"), Times.Once);
 
     }
 
@@ -121,84 +111,74 @@ public class ConsoleAppTests
     public void Run_NumbersWithCustomSeparator_PrintSum()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("//;\\n2;3")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Result: 5"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 5"), Times.Once);
     }
 
     [Fact]
     public void Run_NumbersWithCustomSeparatorAndNewLine_PrintSum()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("//;\\n2;3\\n4")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Result: 9"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 9"), Times.Once);
     }
 
     [Fact]
     public void Run_NumbersWithSeparatorOfAnyLength_PrintSum()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("//[***]\\n1***2***3")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
     }
 
     [Fact]
     public void Run_NumbersWithMultipleCustomSeparators_PrintSum()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("//[*][%]\\n1*2%3")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
-
+        
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
     }
 
     [Fact]
     public void Run_NumbersWithCustomSeparatorAsNestedBrackets_PrintSum()
     {
         // Arrange
-        var consoleWrapper = new Mock<IConsoleWrapper>();
-        consoleWrapper.SetupSequence(x => x.ReadLine())
+        _consoleWrapper.SetupSequence(x => x.ReadLine())
             .Returns("//[**[**]\\n1**[**2**[**3")
             .Returns("");
-        _consoleApp = new ConsoleApp(consoleWrapper.Object);
 
         // Act
         _consoleApp.Run();
 
         // Assert
-        consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
+        _consoleWrapper.Verify(x => x.WriteLine("Result: 6"), Times.Once);
     }
 }
