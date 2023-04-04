71dad75a9846063481aa2b6abb87ab3c0755690b
Yehor Hrabovskyi <egorgrabovskij@gmail.com>
2023-04-04 13:00:31 +0300

    Added console project

---
+++ StringCalculator.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using StringCalculator;
+
+var calculator = new Calculator();
+
+while (true)
+{
+    Console.WriteLine("Enter comma separated numbers(enter to exit):");
+    var input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input))
+    {
+        break;
+    }
+    var result = calculator.Add(input);
+    Console.WriteLine($"Result: {result}");
+}
